d2bdaf5765fcc261705d7651ab6cbaa7357310a3
Ernesto Ojeda Arias <eojeda@gmx.es>
2023-03-01 13:17:18 -0500

    Initial commit

---
+++ src/main/java/guru/springframework/sfgbeerworksbreweryclient/SfgBeerWorksBreweryClientApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgbeerworksbreweryclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgBeerWorksBreweryClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgBeerWorksBreweryClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/sfgbeerworksbreweryclient/SfgBeerWorksBreweryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgbeerworksbreweryclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgBeerWorksBreweryClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
